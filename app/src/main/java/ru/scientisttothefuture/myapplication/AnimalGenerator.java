bd3d3b7b7a04457ab5140d2bd9ab27dd0de7fdaf
levvvchik <levrazuv@icloud.com>
2018-12-14 20:28:15 +0700

    disine of description

--- app/src/main/java/ru/scientisttothefuture/myapplication/AnimalGenerator.java
+++ app/src/main/java/ru/scientisttothefuture/myapplication/AnimalGenerator.java
@@ -6,14 +6,14 @@ import java.util.Random;
 public class AnimalGenerator {
 
     private static final String[] animalNames = new String[]{"cat", "dog", "hamster", "bear", "butterfly"};
-    private static final String[] images = new String[]{"https://cs9.pikabu.ru/post_img/big/2017/02/16/10/1487262267154872190.jpg",
-            "http://img.scoop.it/GroGTPKlZ4COVytp24pJdoXXXL4j3HpexhjNOf_P3YmryPKwJ94QGRtDb3Sbc6KY",
-            "https://storage.googleapis.com/replit/images/1509526663063_78cee07358a3fd43480ae7fa5b4d76e3.png",
-            "https://yt3.ggpht.com/a-/AJLlDp13bM8yFuEE7HVlYHFt7zvZPSMWbGEz4OrxOw=s900-mo-c-c0xffffffff-rj-k-no",
-            "http://comic-cons.xyz/wp-content/uploads/Star-Wars-avatar-icon-Ewok.png",
-            "https://cdn.dribbble.com/users/588874/screenshots/2249423/dribbble_1x.png",
-            "https://avatarko.ru/img/kartinka/32/Star_Wars_pistolet_31680.jpg",
-            "http://weclipart.com/gimg/31A72F8B8986D00C/obiwan_kenobi_rots_by_kaal_jhyy.jpg"};
+    private static final String[] images = new String[]{"https://www.moya-planeta.ru/files/holder/75/fe/75fee17f9ea815b8129e7cafa3271952.png",
+            "https://www.moya-planeta.ru/files/holder/40/5c/405c37827effd8662980ba221d5ab91c.png",
+            "https://www.moya-planeta.ru/files/holder/b7/79/b779d1345477b9580c57a4b0e7cc4331.png",
+            "https://www.moya-planeta.ru/files/holder/32/ac/32ac1e89a9f82d22d1d5881e484e2a6c.png",
+            "https://www.moya-planeta.ru/files/holder/e6/96/e696ab2a92b6ce7cc7375c3c5d653e13.png",
+            "https://www.moya-planeta.ru/files/holder/fc/ee/fcee76b86d180072bff688eeda9d6181.png",
+            "https://www.moya-planeta.ru/files/holder/16/e9/16e976a236b51c03ee822047a4435618.png",
+            "https://www.moya-planeta.ru/files/holder/fc/ee/fcee76b86d180072bff688eeda9d6181.png"};
     private static final String[] locations = new String[]{"Krasnoyarsk","Singapore","London","Moscow","Machachkala"};
 
     private Random random = new Random();
